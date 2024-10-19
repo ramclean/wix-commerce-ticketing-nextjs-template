eca9aacfba3f1383af089928e7ededbcdaebefbf
Ross McLean <rossxmclean@gmail.com>
2024-10-20 00:31:18 +0200

    progress

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Link from 'next/link';
 import Image from 'next/image';
-import { messages } from '@wix/inbox';
 import { useState } from 'react';
 import { FormEvent, ChangeEvent } from 'react';
 
